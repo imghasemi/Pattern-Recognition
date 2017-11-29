77f9cefbfd21cdf4e72493202d8f3bb23b07f1e8
v.karslyan <v.karslyan@corp.mail.ru>
2017-11-29 22:29:07 +0100

    test for whole formula

--- Project 1/Task 1.3/computation_tests.py
+++ Project 1/Task 1.3/computation_tests.py
@@ -10,16 +10,14 @@ The optimisation function calculates the formula after optimisations.
 """
 
 
-def no_optimisation(histogramData):
-    a = 1
-    k = 1
-    return np.sum(((histogramData / a) ** k) * np.log(histogramData / a))
+def dl_dk_init(N, k, a, sum_log_di, histogramData):
+    return N / k - N * np.log(a) + sum_log_di - \
+           np.sum(((histogramData / a) ** k) * np.log(histogramData / a))
 
 
-def optimisation(xValues):
-    a = 1
-    k = 1
-    return np.sum(h * ((xValues / a) ** k) * np.log(xValues / a))
+def dl_dk_optimised(N, k, a, sum_log_di, xValues):
+    return N / k - N * np.log(a) + sum_log_di - \
+           np.sum(h * ((xValues / a) ** k) * np.log(xValues / a))
 
 
 if __name__ == '__main__':
@@ -30,19 +28,31 @@ if __name__ == '__main__':
 
     xValues = np.arange(1, len(h) + 1)
     histogramOfX = []
-    number_of_test = 100
+    number_of_test = 1000
     for i in range(len(h)):
         histogramOfX = np.append(histogramOfX, [xValues[i]] * int(h[i]))
 
+    k = 1
+    a = 1
+    N = len(h)
 
-    print('average time for summations (approach without '
-          'optimisation):\n%1.20f' %
-          (t.timeit('no_optimisation(histogramOfX)',
-                   setup='from __main__ import no_optimisation, histogramOfX',
-                   number=number_of_test)/float(number_of_test)))
+    sum_log_di = np.sum(h * np.log(xValues))
 
-    print('average time for multiplication (optimised approach):\n%1.20f' %
-          (t.timeit('optimisation(xValues)',
-                    setup='from __main__ import optimisation, xValues',
-                    number=number_of_test)/float(number_of_test)))
+    before = t.timeit('dl_dk_init(N, k, a, sum_log_di, histogramOfX)',
+                   setup='from __main__ import dl_dk_init, N, k, a, '
+                         'sum_log_di, histogramOfX',
+                   number=number_of_test)/float(number_of_test)
+
+    after = t.timeit('dl_dk_optimised(N, k, a, sum_log_di, xValues)',
+                    setup='from __main__ import dl_dk_optimised, N, k, a, '
+                          'sum_log_di, xValues',
+                    number=number_of_test)/float(number_of_test)
+    print('Test numbers: %s ' % number_of_test)
+    print('average time for summations '
+          '(approach without optimisation):\n%1.20f' % before)
+    print('average time for multiplication '
+          '(optimised approach):\n%1.20f' % after)
+    print('ratio before/after : %10.10f' % (before/after))
+
+
 
