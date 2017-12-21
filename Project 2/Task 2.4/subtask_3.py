ca6ddf701a44f07ce58f9716ebe4fe68e9751313
imghasemi <in.ghasemi@gmail.com>
2017-12-21 15:12:10 +0100

    Project 2 Completed! [Closed]

--- Project 2/Task 2.4/subtask_3.py
+++ Project 2/Task 2.4/subtask_3.py
@@ -5,25 +5,27 @@
 
 import numpy as np
 
-#least squares from numpy
-def linear_regression(x,y):
+
+# least squares from numpy
+def linear_regression(x, y):
     X = np.column_stack(x + [[1] * len(x[0])])
-    w = np.linalg.lstsq(X, y)[0] #vector w
-    Y = np.dot(X, w) #predicted values
+    w = np.linalg.lstsq(X, y)[0]  # vector w
+    Y = np.dot(X, w)  # predicted values
     return w, Y
 
+
 if __name__ == "__main__":
-    rule110_y = [-1, 1, 1, 1, -1, 1, 1, -1] #y for rule 110
-    rule126_y = [-1, 1, 1, 1, 1, 1, 1, -1] #y for rule 126
-    X = [[[1,1,1,1,1,1,1,1],
-          [1,1,1,-1,1,-1,-1,-1],
-          [1,1,-1,1,-1,1,-1,-1],
-          [1,1,-1,-1,-1,-1,1,1],
-          [1,-1,1,1,-1,-1,1,-1],
-          [1,-1,1,-1,-1,1,-1,1],
-          [1,-1,-1,1,1,-1,-1,1],
-          [1,-1,-1,-1,1,1,1,-1]]
-         ] # now we use matrix big Phi instead of X
+    rule110_y = [-1, 1, 1, 1, -1, 1, 1, -1]  # y for rule 110
+    rule126_y = [-1, 1, 1, 1, 1, 1, 1, -1]  # y for rule 126
+    X = [[[1, 1, 1, 1, 1, 1, 1, 1],
+          [1, 1, 1, -1, 1, -1, -1, -1],
+          [1, 1, -1, 1, -1, 1, -1, -1],
+          [1, 1, -1, -1, -1, -1, 1, 1],
+          [1, -1, 1, 1, -1, -1, 1, -1],
+          [1, -1, 1, -1, -1, 1, -1, 1],
+          [1, -1, -1, 1, 1, -1, -1, 1],
+          [1, -1, -1, -1, 1, 1, 1, -1]]
+         ]  # now we use matrix big Phi instead of X
 
     [w_110, y_110] = linear_regression(X, rule110_y)
     [w_126, y_126] = linear_regression(X, rule126_y)
